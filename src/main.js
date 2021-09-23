cbec9f220b7bb9b5092dd2354765fc80f6add926
Tonny <Tonny@Tonnys-MacBook-Pro.local>
2021-09-23 13:24:44 +0300

    add

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import store from './store'
+
+
 
 import router from './router'
 
@@ -10,6 +13,7 @@ Vue.config.productionTip = false
 
 new Vue({
     router,
+    store,
 
     render: h => h(App),
 }).$mount('#app')
